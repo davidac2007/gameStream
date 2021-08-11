adbfa7db74d004105e05195f504a41c6281a9b6d
David Avendaño <d.avendano.c@hotmail.com>
2021-08-10 21:31:03 -0500

    Initial Commit

---
+++ Shared/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  Shared
+//
+//  Created by David Avendaño on 10/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
