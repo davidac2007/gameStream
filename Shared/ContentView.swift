89e548285a61bb60cfa20d0b6bcc310376977c3f
David Avendaño <d.avendano.c@hotmail.com>
2021-08-11 23:54:56 -0500

    Added first screen into modules

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 19/255, green: 30/255, blue: 53/255, opacity: 1.0)
+            VStack{
+                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250)
+                SignInSignUpView()
+            }
+        }.ignoresSafeArea()
+        
     }
+}
+
+struct SignInSignUpView: View{
+    var body: some View{
+        
+        VStack{
+            HStack {
+                Text("SIGN IN")
+                Text("SIGN UP")
+            }
+        }
+        
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Image("Screen1")
         ContentView()
     }
 }
