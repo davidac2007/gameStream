634ef65eabc70690f1aa34e4bae6664929432389
David Avendaño <d.avendano.c@hotmail.com>
2021-08-12 23:55:50 -0500

    Worked on sign in view with secure field and scroll

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -60,11 +60,43 @@ struct SignInSignUpView: View{
 
 
 struct SignInView: View{
+    
+    @State var email = ""
+    @State var password = ""
     var body: some View{
-        Text("You're in sign in view").foregroundColor(.white)
+        ScrollView {
+            
+            VStack(alignment: .leading){
+                Text("Email").foregroundColor(Color("Dark-Cian"))
+                
+                ZStack(alignment: .leading){
+                    
+                    if email.isEmpty{
+                    Text("example@email.com").font(.caption).foregroundColor(.gray)
+                    }
+                    TextField("", text: $email)
+                }
+                
+                Divider().frame(height: 1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color("Dark-Cian")).padding(.bottom)
+                
+                Text("Password").foregroundColor(Color("Dark-Cian"))
+                ZStack(alignment: .leading){
+                    
+                    if password.isEmpty{
+                    Text("Your password").font(.caption).foregroundColor(.gray)
+                    }
+                    SecureField("", text: $password)
+                }
+                
+                Divider().frame(height: 1, alignment: .center).background(Color("Dark-Cian")).padding(.bottom)
+                
+                
+            }
+        }.padding(.horizontal, 77.0)
        
     }
 }
+
 struct SignUpView: View{
     var body: some View{
         Text("You're in sign up view").foregroundColor(.white)
