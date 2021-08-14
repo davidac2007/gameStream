82905f9fb312608963d166eb8b33bbde8ee0ce4e
David Avendaño <d.avendano.c@hotmail.com>
2021-08-14 16:07:19 -0500

    Divided the UI into modules

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -52,6 +52,7 @@ struct SignInSignUpView: View{
             }else{
                 SignUpView()
             }
+            
         }
         
     }
@@ -60,63 +61,11 @@ struct SignInSignUpView: View{
 
 
 struct SignInView: View{
-    
-    @State var email = ""
-    @State var password = ""
     var body: some View{
         ScrollView {
+            EmailPasswordFields()
+            SocialAuthButtons()
             
-            VStack(alignment: .leading){
-                Text("Email").foregroundColor(Color("Dark-Cian"))
-                
-                ZStack(alignment: .leading){
-                    
-                    if email.isEmpty{
-                        Text("example@email.com").font(.caption).foregroundColor(.gray)
-                    }
-                    TextField("", text: $email)
-                }
-                
-                Divider().frame(height: 1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color("Dark-Cian")).padding(.bottom)
-                
-                Text("Password").foregroundColor(Color(.white))
-                ZStack(alignment: .leading){
-                    
-                    if password.isEmpty{
-                        Text("Your password").font(.caption).foregroundColor(.gray)
-                    }
-                    SecureField("", text: $password)
-                }
-                
-                Divider().frame(height: 1, alignment: .center).background(Color("Dark-Cian")).padding(.bottom)
-                
-                Text("Forgot password?").font(.footnote).frame(width: 300, alignment: .trailing).foregroundColor(Color("Dark-Cian")).padding(.bottom)
-                
-                
-                Button(action: signIn, label: {
-                    Text("SIGN IN").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("Dark-Cian"),lineWidth: 1.0).shadow(color: .white,radius: 6))
-                })
-                
-               
-                
-                Text("Sign in with").font(.body).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
-               
-                
-                HStack(alignment: .center){
-                    Spacer()
-                    Button(action: signInWithFacebook, label: {
-                        Text("Facebook").foregroundColor(.white).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 10, leading: 18, bottom: 11, trailing: 18)).background(RoundedRectangle(cornerRadius: 10.0).fill(Color(red: 41/255, green: 57/255, blue: 87/255, opacity: 1.0)))
-                    })
-                    Spacer(minLength: 20)
-                    Button(action: signInWithTwitter, label: {
-                        Text("Twitter").foregroundColor(.white).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 10, leading: 18, bottom: 11, trailing: 18)).background(RoundedRectangle(cornerRadius: 10.0).fill(Color(red: 41/255, green: 57/255, blue: 87/255, opacity: 1.0)))
-                    })
-                    Spacer()
-                    
-                   
-                }
-                
-            }
         }.padding(.horizontal, 77.0)
         
     }
@@ -132,6 +81,67 @@ func signInWithFacebook(){
 
 func signInWithTwitter(){
     print("Signing in")
+}
+
+struct EmailPasswordFields: View{
+    @State var email = ""
+    @State var password = ""
+    var body: some View{
+        VStack(alignment: .leading){
+            Text("Email").foregroundColor(Color("Dark-Cian"))
+            
+            ZStack(alignment: .leading){
+                
+                if email.isEmpty{
+                    Text("example@email.com").font(.caption).foregroundColor(.gray)
+                }
+                TextField("", text: $email).foregroundColor(.white)
+            }
+            
+            Divider().frame(height: 1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color("Dark-Cian")).padding(.bottom)
+            
+            Text("Password").foregroundColor(Color(.white))
+            ZStack(alignment: .leading){
+                
+                if password.isEmpty{
+                    Text("Your password").font(.caption).foregroundColor(.gray)
+                }
+                SecureField("", text: $password).foregroundColor(.white)
+            }
+            
+            Divider().frame(height: 1, alignment: .center).background(Color("Dark-Cian")).padding(.bottom)
+            
+            Text("Forgot password?").font(.footnote).frame(width: 300, alignment: .trailing).foregroundColor(Color("Dark-Cian")).padding(.bottom)
+            
+            
+            Button(action: signIn, label: {
+                Text("SIGN IN").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("Dark-Cian"),lineWidth: 1.0).shadow(color: .white,radius: 6))
+            })
+            
+          
+        }
+    }
+}
+struct SocialAuthButtons: View{
+    var body: some View{
+        VStack{
+        Text("Sign in with").font(.body).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
+       
+        
+        HStack(alignment: .center){
+            Spacer()
+            Button(action: signInWithFacebook, label: {
+                Text("Facebook").foregroundColor(.white).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 10, leading: 18, bottom: 11, trailing: 18)).background(RoundedRectangle(cornerRadius: 10.0).fill(Color(red: 41/255, green: 57/255, blue: 87/255, opacity: 1.0)))
+            })
+            Spacer(minLength: 20)
+            Button(action: signInWithTwitter, label: {
+                Text("Twitter").foregroundColor(.white).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 10, leading: 18, bottom: 11, trailing: 18)).background(RoundedRectangle(cornerRadius: 10.0).fill(Color(red: 41/255, green: 57/255, blue: 87/255, opacity: 1.0)))
+            })
+            Spacer()
+            
+           
+        }}
+    }
 }
 
 struct SignUpView: View{
