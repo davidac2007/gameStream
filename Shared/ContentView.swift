2cd457f232dc364a142e01fdadbc720861fab89d
David Avendaño <d.avendano.c@hotmail.com>
2021-08-14 20:09:16 -0500

    Completed sign up view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
 
 struct SignInSignUpView: View{
     
-    @State var signIn = true
+    @State var signIn = false
     
     
     var body: some View{
@@ -66,7 +66,7 @@ struct SignInView: View{
             EmailPasswordFields()
             SocialAuthButtons()
             
-        }.padding(.horizontal, 77.0)
+        }.padding(.horizontal, 50.0)
         
     }
 }
@@ -113,15 +113,22 @@ struct EmailPasswordFields: View{
             
             Text("Forgot password?").font(.footnote).frame(width: 300, alignment: .trailing).foregroundColor(Color("Dark-Cian")).padding(.bottom)
             
-            
-            Button(action: signIn, label: {
-                Text("SIGN IN").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("Dark-Cian"),lineWidth: 1.0).shadow(color: .white,radius: 6))
-            })
+            SignInButton()
+           
             
           
         }
     }
 }
+
+struct SignInButton: View{
+    var body: some View{
+        Button(action: signIn, label: {
+            Text("SIGN IN").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("Dark-Cian"),lineWidth: 1.0).shadow(color: .white,radius: 6))
+        })
+    }
+}
+
 struct SocialAuthButtons: View{
     var body: some View{
         VStack{
@@ -144,16 +151,122 @@ struct SocialAuthButtons: View{
     }
 }
 
-struct SignUpView: View{
+struct SignUpButton: View{
     var body: some View{
-        Text("You're in sign up view").foregroundColor(.white)
+        Button(action: signIn, label: {
+            Text("SIGN UP").fontWeight(.bold).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("Dark-Cian"),lineWidth: 1.0).shadow(color: .white,radius: 6))
+        })
+    }
+}
+
+struct SignUpView: View{
+   
+    
+    var body: some View{
+        ScrollView {
+            
+            Text("Choose a profile picture")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
+            
+            Text("You can change it later on")
+                .font(.footnote)
+                .fontWeight(.light)
+                .foregroundColor(.gray)
+            
+            Button(action: takeProfilePic, label: {
+               ZStack {
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                Image(systemName: "camera").foregroundColor(.white)
+                }
+            })
+            
+            VStack(alignment: .leading){
+            
+            
+            TextFields()
+            
+            SignUpButton()
+            SignUpSocialAuthButtons()
+            }
+        }.padding(.horizontal, 30.0)
         
     }
 }
 
+struct TextFields: View{
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View{
+        Text("Email*").foregroundColor(Color("Dark-Cian"))
+        
+        ZStack(alignment: .leading){
+            
+            if email.isEmpty{
+                Text("example@email.com").font(.caption).foregroundColor(.gray)
+            }
+            TextField("", text: $email).foregroundColor(.white)
+        }
+        
+        Divider().frame(height: 1, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color("Dark-Cian")).padding(.bottom)
+        
+        Text("Password*").foregroundColor(Color(.white))
+        ZStack(alignment: .leading){
+            
+            if password.isEmpty{
+                Text("Your password").font(.caption).foregroundColor(.gray)
+            }
+            SecureField("", text: $password).foregroundColor(.white)
+        }
+        
+        Divider().frame(height: 1, alignment: .center).background(Color("Dark-Cian")).padding(.bottom)
+            
+            Text("Confirm Password*").foregroundColor(Color(.white))
+            ZStack(alignment: .leading){
+                
+                if password.isEmpty{
+                    Text("Confirm your password").font(.caption).foregroundColor(.gray)
+                }
+                SecureField("", text: $password).foregroundColor(.white)
+            }
+            
+            Divider().frame(height: 1, alignment: .center).background(Color("Dark-Cian")).padding(.bottom, 70.0)
+    }
+}
+
+struct SignUpSocialAuthButtons: View{
+    var body: some View{
+        VStack{
+        Text("Sign up with").font(.body).foregroundColor(.white).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 50, leading: 0, bottom: 10, trailing: 0))
+       
+        
+        HStack(alignment: .center){
+            Spacer()
+            Button(action: signInWithFacebook, label: {
+                Text("Facebook").foregroundColor(.white).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 10, leading: 18, bottom: 11, trailing: 18)).background(RoundedRectangle(cornerRadius: 10.0).fill(Color(red: 41/255, green: 57/255, blue: 87/255, opacity: 1.0)))
+            })
+            Spacer(minLength: 20)
+            Button(action: signInWithTwitter, label: {
+                Text("Twitter").foregroundColor(.white).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).frame(maxWidth: .infinity, alignment: .center).padding(EdgeInsets(top: 10, leading: 18, bottom: 11, trailing: 18)).background(RoundedRectangle(cornerRadius: 10.0).fill(Color(red: 41/255, green: 57/255, blue: 87/255, opacity: 1.0)))
+            })
+            Spacer()
+            
+           
+        }}
+    }
+}
+
+func takeProfilePic() {
+    print("Taking picture")
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Image("Screen1")
+        Image("Screen2")
         ContentView()
     }
 }
