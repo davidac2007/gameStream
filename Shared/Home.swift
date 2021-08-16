9c358c0f6d7bd17a7952c7b8d1ab5e20ea188df9
David Avendaño <d.avendano.c@hotmail.com>
2021-08-15 20:46:52 -0500

    Created Home View

--- Shared/Home.swift
+++ Shared/Home.swift
@@ -8,39 +8,63 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var selectedTab: Int = 2
     var body: some View {
         
-        TabView{
+        TabView(selection: $selectedTab){
             Text("Profile Screen").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     
                     Image(systemName: "person")
                     Text("Profile")
-                }
+                }.tag(0)
+            
             Text("Games Screen").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     
                     Image(systemName: "gamecontroller")
                     Text("Games")
-                }
+                }.tag(1)
             
-            Text("Home Screen").font(.system(size: 30, weight: .bold, design: .rounded))
+          HomeScreen()
                 .tabItem {
                     
                     Image(systemName: "house")
                     Text("Home")
-                }
+                }.tag(2)
             
             Text("Favorites Screen").font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     
                     Image(systemName: "heart")
                     Text("Favorites")
-                }
-        }
+                }.tag(3)
+        }.accentColor(.white)
         
+    }
+    
+    init() {
+        UITabBar.appearance().barTintColor = UIColor(Color("TabBarColor"))
+        UITabBar.appearance().isTranslucent = true
+        print("init views")
+    }
+}
+
+struct HomeScreen: View{
+    var body: some View{
         
-        
+        ZStack {
+            
+            Color("NavyBlue").ignoresSafeArea()
+            
+            VStack {
+                Text("Hello").foregroundColor(.white)
+            }.padding(.horizontal, 18)
+            
+            
+            
+        }.navigationBarHidden(true).navigationBarBackButtonHidden(true)
     }
 }
 
