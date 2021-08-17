54a33eef989e6e83f12cfa7e7ab9c547df694063
David Avendaño <d.avendano.c@hotmail.com>
2021-08-17 11:02:55 -0500

    Added some carousels to home screen

--- Shared/Home.swift
+++ Shared/Home.swift
@@ -28,7 +28,7 @@ struct Home: View {
                     Text("Games")
                 }.tag(1)
             
-          HomeScreen()
+            HomeScreen()
                 .tabItem {
                     
                     Image(systemName: "house")
@@ -64,7 +64,7 @@ struct HomeScreen: View{
             
             ScrollView {
                 VStack {
-                
+                    
                     Image("AppLogo")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
@@ -82,14 +82,14 @@ struct HomeScreen: View{
                         ZStack(alignment: .leading){
                             if textSearch.isEmpty{
                                 Text("Search")
-                                .foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0))
+                                    .foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0))
                             }
                             
                             TextField("", text: $textSearch).foregroundColor(.white)
                         }
                         
                     }.padding([.top, .leading, .bottom], 11.0)
-                        .background(Color("Blue-Gray")).clipShape(Capsule())
+                    .background(Color("Blue-Gray")).clipShape(Capsule())
                     
                 }.padding(.horizontal, 18)
                 SubModuleHome()
@@ -107,9 +107,9 @@ struct HomeScreen: View{
 }
 
 struct SubModuleHome:View{
-     @State var isPlayerActive = false
-     @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
-     let urlVideos:[String] = ["https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"]
+    @State var isPlayerActive = false
+    @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
+    let urlVideos:[String] = ["https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"]
     var body: some View{
         
         
@@ -144,6 +144,77 @@ struct SubModuleHome:View{
                 
             }.frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity,  alignment: .center)
             .padding(.vertical)
+            
+            Text("Explore")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity,  alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack{
+                    
+                    Button(action: {}, label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            Image("FPS").resizable().aspectRatio(contentMode: .fit).frame(width: 42, height: 42)
+                        }
+                    })
+                    Button(action: {}, label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            Image("RPG").resizable().aspectRatio(contentMode: .fit).frame(width: 42, height: 42)
+                        }
+                    })
+                    Button(action: {}, label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            Image("OpenWorld").resizable().aspectRatio(contentMode: .fit).frame(width: 42, height: 42)
+                        }
+                    })
+                }
+                
+            }
+            
+            Text("Recommended for you")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,maxWidth: .infinity,  alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack{
+                    
+                    Button(action: {url = urlVideos[1]
+                            print("url: \(url)")
+                            isPlayerActive = true}, label: {
+                                Image("Abzu").resizable().scaledToFit().frame(width: 240, height: 135)
+                    })
+                    
+                    Button(action: {url = urlVideos[2]
+                            print("url: \(url)")
+                            isPlayerActive = true}, label: {
+                                Image("Crash Bandicoot").resizable().scaledToFit().frame(width: 240, height: 135)
+                    })
+                    
+                    Button(action: {url = urlVideos[3]
+                            print("url: \(url)")
+                            isPlayerActive = true}, label: {
+                                Image("DEATH STRANDING").resizable().scaledToFit().frame(width: 240, height: 135)
+                    })
+                 
+                }
+                
+            }
+            
+            
+            
         }.padding(.horizontal, 20)
         
         NavigationLink(
