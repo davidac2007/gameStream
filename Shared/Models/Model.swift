a9d2d303a27a05035f75c736d7ab45f165a934a3
David Avendaño <d.avendano.c@hotmail.com>
2021-08-17 22:37:11 -0500

    Modeling JSON Data

--- Shared/Models/Model.swift
+++ Shared/Models/Model.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct Games: Codable{
+    
+    var games:[Game]
+}
+
+struct Game: Codable{
+    
+    var title:String
+    var studio:String
+    var contentRating:String
+    var publicationYear:String
+    var description:String
+    var platforms:[String]
+    var tags:[String]
+    var videosUrls:VideoUrl
+    var galleryImages:[String]
+    
+}
+
+struct VideoUrl:Codable{
+    var mobile:String
+    var tablet:String
+}
