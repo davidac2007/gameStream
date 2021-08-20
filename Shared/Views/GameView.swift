82b3b699686b4451bc3aaf585193b6274e748a10
David Avendaño <d.avendano.c@hotmail.com>
2021-08-20 13:42:52 -0500

    Added initial values to Canvas

--- Shared/Views/GameView.swift
+++ Shared/Views/GameView.swift
@@ -22,12 +22,25 @@ struct GameView: View {
     var body: some View {
         
         
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("NavyBlue").ignoresSafeArea()
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+            }
+        }
     }
 }
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        GameView()
+        GameView(
+            url: "Ejemplo.com",
+            title: "Sonic",
+            studio: "Sega",
+            contentRating: "E+",
+            publicationYear: "1991",
+            description: "Juego de Sega Genesis publicado en 1991 con más de 40 millones de copias vendidas actualmente",
+            tags: ["plataformas", "mascotas"],
+            imagesUrl: ["https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg","https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg","https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg"])
     }
 }
