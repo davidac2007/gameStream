bc2b9a5c9c18c77c5972e318e72fe872ec8cada4
David Avendaño <d.avendano.c@hotmail.com>
2021-08-21 23:41:37 -0500

    Added Similar Games Section

--- Shared/Views/GameView.swift
+++ Shared/Views/GameView.swift
@@ -47,6 +47,7 @@ struct GameView: View {
                         
                         Gallery(imagesUrl: imagesUrl)
                         Comments().padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                        SimilarGames()
                     }.frame(maxWidth: .infinity)
                 }
             }
@@ -123,6 +124,52 @@ struct Comments: View{
             }.clipShape(RoundedRectangle(cornerRadius: 8))
             
         }.frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct SimilarGames: View{
+    
+    let urlVideos:[String] = ["https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"]
+    @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
+    var body: some View{
+        
+        VStack(alignment: .leading){
+        Text("Similar Games")
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding(.leading)
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack(spacing: 15){
+                    
+                    Button(action: {url = urlVideos[4]
+                          
+//                            isPlayerActive = true
+                        
+                    }, label: {
+                                Image("Cuphead").resizable().scaledToFit().frame(width: 240, height: 135)
+                    })
+                    
+                    Button(action: {url = urlVideos[5]
+                          
+//                            isPlayerActive = true
+                        
+                    }, label: {
+                                Image("Hades").resizable().scaledToFit().frame(width: 240, height: 135)
+                    })
+                    
+                    Button(action: {url = urlVideos[6]
+                       
+//                            isPlayerActive = true
+                        
+                    }, label: {
+                                Image("Grand Theft Auto V").resizable().scaledToFit().frame(width: 240, height: 135)
+                    })
+                 
+                }
+                
+            }.padding(.bottom)
+            
+        }
     }
 }
 
